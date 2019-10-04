d59d6592cd9ca9f6ded9ab9a736ac06001c3b384
rmejia-tajc <rmejia.tajc@gmail.com>
2019-10-04 14:16:37 -0600

    complete ex2.py. all test pass

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -1,9 +1,7 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+                        hash_table_retrieve)
 
 
 class Ticket:
@@ -19,5 +17,17 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
+    # loop through all tickets...
+    for i in range(length):
+        # if the current ticket's does not have a source...
+        if tickets[i].source == "NONE":
+            # then this is the first flight. Add it to the beginning of route
+            route[0] = tickets[i].destination
+        # add the current value to hashtable (key=source, value=destination)
+        hash_table_insert(hashtable, tickets[i].source, tickets[i].destination)
+    
+    for t in range(length):
+        if route[t-1] is not None:
+            route[t] = hash_table_retrieve(hashtable, route[t-1])
 
     return route
